87e350f75500b7124bc673e61f45e5c3e73cd748
Antonina Lehka <lehka.a@dbbest.com>
2017-11-10 15:56:16 +0200

    added first animation

---
+++ app/src/main/java/com/dbbest/animatebuttons/FabActivity.java
@@ -0,0 +1,503 @@
+package com.dbbest.animatebuttons;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.Transition;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+public class FabActivity extends AppCompatActivity {
+
+    int height;
+    int width;
+    ConstraintLayout constraintLayout;
+    int duration = 4000;
+    Transition sharedElementEnterTransition;
+    Transition.TransitionListener transitionListener;
+    private ImageView closeButton;
+    private ImageView transpButton;
+    private FloatingActionButton editButton;
+    private Animation show_edit;
+    private Animation show_compass;
+    private Animation show_myplaces;
+    private Animation show_share;
+    private Animation show_add;
+    private Animation show_view;
+    private Animation show_camera;
+    private FrameLayout layoutFabs;
+
+
+    @Override
+    public void onBackPressed() {
+
+        sharedElementEnterTransition.removeListener(transitionListener);
+        setAnim(constraintLayout, false);
+        setFab(closeButton, true);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fabs);
+        layoutFabs = findViewById(R.id.layout_fabs);
+
+        closeButton = findViewById(R.id.fab_repaint_close);
+        constraintLayout = findViewById(R.id.bg);
+
+        final FloatingActionButton compassButton = findViewById(R.id.fab_compass);
+        final FloatingActionButton myplacesButton = findViewById(R.id.fab_myplaces);
+        final FloatingActionButton shareButton = findViewById(R.id.fab_share);
+        final FloatingActionButton addButton = findViewById(R.id.fab_add);
+        final FloatingActionButton viewButton = findViewById(R.id.fab_view);
+        final FloatingActionButton cameraButton = findViewById(R.id.fab_camera);
+
+        editButton = findViewById(R.id.fab_edit);
+
+        show_edit = AnimationUtils.loadAnimation(getApplication(), R.anim.edit_show);
+        final Animation hide_edit = AnimationUtils.loadAnimation(getApplication(), R.anim.edit_hide);
+//
+
+//
+        show_compass = AnimationUtils.loadAnimation(getApplication(), R.anim.compass_show);
+        final Animation hide_compass = AnimationUtils.loadAnimation(getApplication(), R.anim.compass_hide);
+//
+//
+        show_myplaces = AnimationUtils.loadAnimation(getApplication(), R.anim.myplaces_show);
+        final Animation hide_myplaces = AnimationUtils.loadAnimation(getApplication(), R.anim.myplaces_hide);
+//
+        show_share = AnimationUtils.loadAnimation(getApplication(), R.anim.share_show);
+        final Animation hide_share = AnimationUtils.loadAnimation(getApplication(), R.anim.share_hide);
+//
+        show_add = AnimationUtils.loadAnimation(getApplication(), R.anim.add_show);
+        final Animation hide_add = AnimationUtils.loadAnimation(getApplication(), R.anim.add_hide);
+//
+        show_view = AnimationUtils.loadAnimation(getApplication(), R.anim.view_show);
+        final Animation hide_view = AnimationUtils.loadAnimation(getApplication(), R.anim.view_hide);
+//
+        show_camera = AnimationUtils.loadAnimation(getApplication(), R.anim.camera_show);
+        final Animation hide_camera = AnimationUtils.loadAnimation(getApplication(), R.anim.camera_hide);
+
+//        final Animation rotateAnim = AnimationUtils.loadAnimation(getApplication(), R.anim
+//                .appear_fab_close);
+
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myplacesButton.startAnimation(hide_myplaces);
+                myplacesButton.setClickable(false);
+                sharedElementEnterTransition.removeListener(transitionListener);
+            }
+        });
+
+
+        getWindow().setEnterTransition(null);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        height = displayMetrics.heightPixels;
+        width = displayMetrics.widthPixels;
+
+
+        sharedElementEnterTransition = getWindow().getSharedElementEnterTransition();
+
+
+        transitionListener = new Transition.TransitionListener() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+
+
+//                rotateAnim.setAnimationListener(new Animation.AnimationListener() {
+//                    @Override
+//                    public void onAnimationStart(Animation animation) {
+//
+//
+//                    }
+//
+//                    @Override
+//                    public void onAnimationEnd(Animation animation) {
+//                        ValueAnimator positionAnimator = ValueAnimator.ofFloat(-26f, 30f);
+//// 2
+//                        positionAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+//                            @Override
+//                            public void onAnimationUpdate(ValueAnimator animation) {
+//                                transpButton.setTranslationY((Float) (animation.getAnimatedValue()));
+//                            }
+//                        });
+//// 3
+//                        ObjectAnimator rotationAnimator = ObjectAnimator.ofFloat(transpButton,
+//                                "rotation", 0f, 360f);
+//// 4
+//                        AnimatorSet animatorSet = new AnimatorSet();
+//// 5
+//                        animatorSet.play(positionAnimator).with(rotationAnimator);
+//// 6
+//                        animatorSet.setDuration(4000);
+//                        animatorSet.start();
+//
+//                    }
+//
+//                    @Override
+//                    public void onAnimationRepeat(Animation animation) {
+//
+//                    }
+//                });
+
+
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                setAnim(constraintLayout, true);
+                setFab(closeButton, false);
+            }
+
+            @Override
+            public void onTransitionCancel(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionPause(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionResume(Transition transition) {
+
+            }
+        };
+
+        sharedElementEnterTransition.addListener(transitionListener);
+
+
+        FrameLayout.LayoutParams myplacesParams = (FrameLayout.LayoutParams) myplacesButton.getLayoutParams();
+        myplacesParams.rightMargin += 0;
+        myplacesParams.bottomMargin += (int) (myplacesButton.getHeight() * 2.5);
+        myplacesButton.setLayoutParams(myplacesParams);
+        myplacesButton.startAnimation(show_myplaces);
+        myplacesButton.setClickable(true);
+
+        show_myplaces.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                cameraButton.startAnimation(show_camera);
+                cameraButton.setClickable(true);
+            }
+
+        });
+
+        hide_myplaces.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                cameraButton.startAnimation(hide_camera);
+                cameraButton.setClickable(false);
+            }
+
+        });
+
+        show_camera.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                shareButton.startAnimation(show_share);
+                shareButton.setClickable(true);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        hide_camera.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                shareButton.startAnimation(hide_share);
+                shareButton.setClickable(false);
+            }
+
+        });
+
+        show_share.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                addButton.startAnimation(show_add);
+                addButton.setClickable(true);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        hide_share.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                addButton.startAnimation(hide_add);
+                addButton.setClickable(false);
+            }
+
+        });
+
+        show_add.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                viewButton.startAnimation(show_view);
+                viewButton.setClickable(true);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        hide_add.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                viewButton.startAnimation(hide_view);
+                viewButton.setClickable(false);
+            }
+
+        });
+
+
+        show_view.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                compassButton.startAnimation(show_compass);
+                compassButton.setClickable(true);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        hide_view.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                compassButton.startAnimation(hide_compass);
+                compassButton.setClickable(true);
+            }
+
+        });
+        hide_compass.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                editButton.startAnimation(hide_edit);
+                editButton.setClickable(true);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        hide_edit.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                setAnim(constraintLayout, false);
+                setFab(closeButton, true);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void setAnim(final View myView, boolean isShow) {
+
+// get the center for the clipping circle
+        int cx = closeButton.getWidth() / 2;
+        int cy = closeButton.getHeight() / 2;
+
+// get the final radius for the clipping circle
+        float finalRadius = (float) Math.hypot(width, height);
+
+        int[] startingLocation = new int[2];
+        closeButton.getLocationInWindow(startingLocation);
+
+// create the animator for this view (the start radius is zero)
+        Animator anim;
+        if (isShow) {
+            anim =
+                    ViewAnimationUtils.createCircularReveal(myView, (int) (closeButton.getX() + cx), (int) (closeButton.getY() + cy), 0, finalRadius);
+            // make the view visible and start the animation
+            myView.setVisibility(View.VISIBLE);
+        } else {
+            anim =
+                    ViewAnimationUtils.createCircularReveal(myView, (int) (closeButton.getX() + cx), (int) (closeButton.getY() + cy), finalRadius, 0);
+            anim.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
+
+                    myView.setVisibility(View.INVISIBLE);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+
+                }
+            });
+        }
+
+        anim.setDuration(duration);
+        anim.start();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    void setFab(final View myView, boolean isShow) {
+
+// get the center for the clipping circle
+        int cx = myView.getWidth() / 2;
+        int cy = myView.getHeight() / 2;
+
+// get the initial radius for the clipping circle
+        float initialRadius = (float) Math.hypot(cx, cy);
+        Animator anim;
+        if (isShow) {
+// create the animation (the final radius is zero)
+            anim =
+                    ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0, initialRadius);
+// make the view invisible when the animation is done
+            anim.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    myView.setVisibility(View.VISIBLE);
+
+                    finishAfterTransition();
+
+                }
+            });
+            anim.setDuration(duration);
+        } else {
+
+            anim =
+                    ViewAnimationUtils.createCircularReveal(myView, cx, cy, initialRadius, 0);
+// make the view invisible when the animation is done
+            anim.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+//                    myView.setVisibility(View.INVISIBLE);
+                    layoutFabs.setVisibility(View.VISIBLE);
+                    if (editButton != null) {
+
+                        editButton.startAnimation(show_edit);
+                    }
+
+                }
+            });
+        }
+// start the animation
+        anim.start();
+
+    }
+
+
+}
--- app/src/main/java/com/dbbest/animatebuttons/MainActivity.java
+++ app/src/main/java/com/dbbest/animatebuttons/MainActivity.java
@@ -1,13 +1,71 @@
 package com.dbbest.animatebuttons;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView fab;
+    private ImageView fabClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fab = findViewById(R.id.fab_main);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final Intent intent = new Intent(MainActivity.this, FabActivity.class);
+
+
+                ActivityOptions options;
+//                fab.setBackgroundResource(R.drawable.shape_create_edit_bg);
+                options = ActivityOptions.makeSceneTransitionAnimation(
+                        MainActivity.this,
+                        android.util.Pair.create((View) fab, "bg"));
+                startActivity(intent, options.toBundle());
+//                fab.setVisibility(View.GONE);
+
+//                final Animation animAppear = AnimationUtils.loadAnimation(MainActivity.this, R.anim.appear_fab_close);
+//                final Animation animDisappear = AnimationUtils.loadAnimation(MainActivity.this, R.anim.edit_hide);
+//                fabClose.setVisibility(View.VISIBLE);
+//                fabClose.startAnimation(animAppear);
+//
+//
+//                animAppear.setAnimationListener(new Animation.AnimationListener() {
+//                    @Override
+//                    public void onAnimationStart(Animation animation) {
+//                        fab.setBackgroundResource(R.drawable.shape_create_edit_bg);
+////                        fab.startAnimation(animDisappear);
+//                    }
+//
+//                    @Override
+//                    public void onAnimationEnd(Animation animation) {
+//
+//                        ActivityOptions options;
+//                        options = ActivityOptions.makeSceneTransitionAnimation(
+//                                MainActivity.this,
+//                                android.util.Pair.create((View) fab, "bg"));
+//                        fab.setVisibility(View.GONE);
+//                        fabClose.setVisibility(View.GONE);
+//
+//                    }
+//
+//
+//                    @Override
+//                    public void onAnimationRepeat(Animation animation) {
+//
+//                    }
+//                });
+
+            }
+        });
     }
+
+
 }
